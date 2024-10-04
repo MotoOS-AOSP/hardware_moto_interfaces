31de1c219f7f34fd3572ef16a5e695f576acaa37
seba3567 <sebastian26481@gmail.com>
2024-10-04 13:40:43 -0300

    moto Init

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.lineage:hardware/blaze/interfaces" \
+  "vendor.lineage:hardware/moto/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
